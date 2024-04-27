0c4c2c5950f5f87199b754c60f3168b462fea50b
aaminariyaz <160913667+aaminariyaz@users.noreply.github.com>
2024-04-27 09:08:36 +0000

    switch statement

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,48 @@
+// Switch syntax
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3
+// switch (month) {
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 1:
+//         console.log("February");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     case 4:
+//         console.log("April");
+//     default:
+//         console.log("default case match");
+//         break;
+// }
+
+
+const month = "apr"
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("February");
+        break;
+    case "mar":
+        console.log("March");
+        break;
+    case "apr":
+        console.log("April");
+        break;
+    default:
+        console.log("default case match");
+        break;
+}
