0897a0edf974eef49afca1d2372f8092bff48c1c
aaminariyaz <160913667+aaminariyaz@users.noreply.github.com>
2024-04-27 11:16:57 +0000

    while , do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+// while
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+myArray = ["Superman" , "Ironman" , "Hulk"]
+let i = 0
+while (i < myArray.length) {
+    console.log(myArray[i]);
+    i = i+1
+}
+
+let score = 11
+do {
+    console.log(`Score is ${score}`);
+    score = score + 1
+} while (score <=10);
